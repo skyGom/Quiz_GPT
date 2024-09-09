80bb133e7dfa1c83bef9c0054462024e2220d128
skyGom <dprha4789@gmail.com>
2024-09-09 10:04:40 +0900

    type ignore 설정

--- gpt/retrievers.py
+++ gpt/retrievers.py
@@ -1,6 +1,6 @@
 from langchain.retrievers import WikipediaRetriever
 
 def wiki_search(topic):
-    retriever = WikipediaRetriever(top_k_results=5)
+    retriever = WikipediaRetriever(top_k_results=5) # type: ignore
     docs = retriever.get_relevant_documents(topic)
     return docs
